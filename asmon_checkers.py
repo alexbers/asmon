cb153cb62e1b698af2ca240bd57edb49811fa1c5
Alexander Bersenev <bay@hackerdom.ru>
2023-03-26 04:03:36 +0500

    replace aiohttp with httpx

--- asmon_checkers.py
+++ asmon_checkers.py
@@ -5,10 +5,12 @@
 import asyncio
 import time
 
-import aiohttp
+import httpx
 
 from asmon import alert
 
+
+client = None
 
 async def check_tcp_port(host, port):
     writer = None
@@ -21,43 +23,41 @@ async def check_tcp_port(host, port):
             writer.close()
 
 
-async def check_cert_expire(host, days=7, timeout=10):
-    expire_time = 0
-    class MyConnector(aiohttp.TCPConnector):
-        async def connect(self, *args, **kwargs):
-            TIME_FMT = "%b %d %H:%M:%S %Y %Z"
-
-            c = await aiohttp.TCPConnector.connect(self, *args, **kwargs)
-            sslobj = c.transport.get_extra_info("ssl_object")
-            cert = sslobj.getpeercert()
-
-            nonlocal expire_time
-            expire_time = time.mktime(time.strptime(cert["notAfter"], TIME_FMT))
-            return c
+async def check_cert_expire(host, days=7, timeout=30):
+    global client
     try:
-        timeout = aiohttp.ClientTimeout(total=timeout)
-        async with aiohttp.ClientSession(connector=MyConnector(), timeout=timeout) as session:
-            async with session.head(f'https://{host}/') as resp:
-                pass
-    except aiohttp.client_exceptions.ClientConnectorCertificateError as E:
-        alert(f"битый сертификат {host}: {E.certificate_error}", 2)
-    except aiohttp.client_exceptions.ClientConnectorError as E:
-        alert(f"не получилось подключиться к {host}: {E.os_error}", 2)
-    except aiohttp.client_exceptions.ServerTimeoutError as E:
+        if client is None:
+            timeout = httpx.Timeout(timeout, pool=timeout*10)
+            client = httpx.AsyncClient(timeout=timeout)
+
+        r = await client.head(f"https://{host}/", follow_redirects=False)
+
+        network_stream = r.extensions["network_stream"]
+        ssl_object = network_stream.get_extra_info("ssl_object")
+        cert = ssl_object.getpeercert()
+
+        TIME_FMT = "%b %d %H:%M:%S %Y %Z"
+        expire_time = time.mktime(time.strptime(cert["notAfter"], TIME_FMT))
+
+        time_left = int(expire_time - time.time())
+        if time_left < 0:
+            alert(f"сертификат {host} закончился {-time_left//60//60//24} дн. назад", 1)
+        elif time_left < 60*60*24*days:
+            alert(f"сертификат {host} закончится через {time_left//60//60//24} дн.", 1)
+
+    except asyncio.TimeoutError as E:
         alert(f"не получилось подключиться по https к {host}: таймаут", 2)
+    except Exception as E:
+        err = str(E)
+        if not err:
+            err = type(E).__name__
+        alert(f"не получилось подключиться по https к {host}: {err}", 2)
 
-    if not expire_time:
-        return
-
-    time_left = int(expire_time - time.time())
-    if time_left < 0:
-        alert(f"сертификат {host} закончился {-time_left//60//60//24} дн. назад", 1)
-    elif time_left < 60*60*24*days:
-        alert(f"сертификат {host} закончится через {time_left//60//60//24} дн.", 1)
 
 if __name__ == "__main__":
-    # print("MAIN")
-    asyncio.run(check_cert_expire("expired.badssl.com"))
+    print("MAIN")
+    # asyncio.run(check_cert_expire("alexbers.com"))
+    # asyncio.run(check_cert_expire("expired.badssl.com"))
     # asyncio.run(check_cert_expire("wrong.host.badssl.com"))
     # asyncio.run(check_cert_expire("self-signed.badssl.com"))
     # asyncio.run(check_cert_expire("untrusted-root.badssl.com"))
