bf6161009029a67cd27416e1c8a16f43de687494
Alexander Bersenev <bay@hackerdom.ru>
2023-09-01 05:02:43 +0500

    better retry logic

--- check_example.py
+++ check_example.py
@@ -26,7 +26,7 @@ async def just_check():
     alert("this is a test alert, please edit check_example.py")
 
 
-# @checker(pause=5, timeout=20)
+# @checker(pause=5, timeout=600)
 # async def check_google_port80():
 #     """
 #     An example of basic checker, checks if TCP port is open every 5 seconds.
@@ -35,7 +35,7 @@ async def just_check():
 #     await common_checks.check_tcp_port("google.com", 80)
 
 
-# @checker(args=["ya.ru", "google.com"], pause=1*60*60, timeout=60)
+# @checker(args=["ya.ru", "google.com"], pause=1*60*60, timeout=600)
 # async def check_certs(host):
 #     """
 #     You can specify several hosts to check, every arg is a task
@@ -43,7 +43,7 @@ async def just_check():
 #     await common_checks.check_cert_expire(host, days=10000)
 
 
-# @checker(pause=60, alerts_repeat_after=60*60*48, timeout=30)
+# @checker(pause=60, alerts_repeat_after=60*60*48, timeout=600)
 # async def check_rest_api():
 #     """
 #     More complex checks, shows how easy you can write custom checks
@@ -51,7 +51,7 @@ async def just_check():
 #     """
 #     try:
 #         async with httpx.AsyncClient() as client:
-#             resp = await client.get("https://reqres.in/api/users", timeout=10)
+#             resp = await client.get("https://reqres.in/api/users", timeout=300)
 #             if resp.status_code != 200:
 #                 alert(f"тестовый rest-сервис вернул плохой статус ответа {resp.status_code}")
 #                 return
--- common_checks.py
+++ common_checks.py
@@ -8,10 +8,20 @@ import time
 from asmon import alert
 
 
-async def check_tcp_port(host, port, timeout=30):
+async def wait_for_with_attempts(task, timeout, attempts):
+    for attempt in range(attempts):
+        try:
+            return await asyncio.wait_for(task, timeout=timeout)
+        except TimeoutError:
+            if attempt == attempts - 1:
+                raise
+
+
+async def check_tcp_port(host, port, timeout=10, attempts=30):
     writer = None
     try:
-        reader, writer = await asyncio.wait_for(asyncio.open_connection(host, port), timeout=timeout)
+        reader, writer = await wait_for_with_attempts(
+            asyncio.open_connection(host, port), timeout=timeout, attempts=attempts)
     except (OSError, TimeoutError, asyncio.exceptions.TimeoutError) as E:
         err = str(E)
         if not err:
@@ -22,11 +32,12 @@ async def check_tcp_port(host, port, timeout=30):
             writer.close()
 
 
-async def check_cert_expire(host, port=443, days=7, timeout=30):
+async def check_cert_expire(host, port=443, days=7, timeout=10, attempts=30):
     writer = None
     try:
-        reader, writer = await asyncio.wait_for(
-            asyncio.open_connection(host, port, ssl=True, server_hostname=host, limit=4096), timeout=timeout)
+        reader, writer = await wait_for_with_attempts(
+            asyncio.open_connection(
+                host, port, ssl=True, server_hostname=host, limit=4096), timeout=timeout, attempts=attempts)
         ssl_object = writer.get_extra_info("ssl_object")
         cert = ssl_object.getpeercert()
 
