afeaa4ad323cd3e5ad1ced44c6c86728a87f00fd
Alexander Bersenev <bay@hackerdom.ru>
2023-12-10 01:27:41 +0500

    refactor alerts logic

--- asmon/alerts.py
+++ asmon/alerts.py
@@ -66,29 +66,9 @@ def recover_alerts(filename, unregistered_only=False):
             if alert.filename != filename:
                 continue
 
-        if alert.prefix not in prefix_to_checks_cnt or not unregistered_only:
-            alert.last_update_time = time.time()
-            alert.recovered = True
-
-
-async def send_msg(user_id, text):
-    log("send_msg", user_id, text)
-
-    url = "https://api.telegram.org/bot%s/sendMessage" % BOT_TOKEN
-    payload = {"chat_id": user_id, "text": text,}
-
-    try:
-        async with httpx.AsyncClient() as client:
-            resp = httpx.post(url, json=payload)
-            if resp.status_code != 200:
-                log(f"Failed to send msg to {user_id}: {text} " +
-                    f"{resp.status_code} {resp.text}")
-                metrics.tg_fails += 1
-            return resp.status_code == 200
-    except OSError:
-        traceback.print_exc()
-        metrics.exceptions_cnt["alert_sender"] += 1
-        return False
+            if alert.prefix not in prefix_to_checks_cnt or not unregistered_only:
+                alert.last_update_time = time.time()
+                alert.recovered = True
 
 
 def alert(text, alert_id="default", renotify=None, if_in_a_row=None):
@@ -106,15 +86,12 @@ def alert(text, alert_id="default", renotify=None, if_in_a_row=None):
     if renotify is None:
         renotify = renotify_ctx.get()
 
-    if len(text) > MAX_ALERT_MSG_LEN:
-        text = text[:MAX_ALERT_MSG_LEN-3] + "..."
-
     prefix = prefix_ctx.get()
     id_to_alert = prefix_to_id_to_alert[prefix]
     filename, funcname, funcarg = prefix
 
     if alert_id not in id_to_alert:
-        id_to_alert[alert_id] = Alert(alert_id, text, filename=filename,
+        id_to_alert[alert_id] = Alert(alert_id=alert_id, text=text, filename=filename,
                                       funcname=funcname, funcarg=funcarg,
                                       start_time=time.time(),
                                       last_update_time=time.time(), last_send_time=0,
@@ -142,6 +119,32 @@ def format_seconds(sec, lang="EN"):
         return f"{sec//60//60//24} дн." if lang == "RU" else f"{sec} days."
 
 
+def format_alert(alert, lang="EN", max_len=MAX_ALERT_MSG_LEN):
+    text = ""
+    if alert.recovered:
+        broken_time = format_seconds(time.time() - alert.start_time, lang=lang)
+        if lang == "RU":
+            text = f"🎉 починилось, было сломано {broken_time}: {alert.text}"
+        else:
+            text = f"🎉 fixed, was broken {broken_time}: {alert.text}"
+    elif alert.last_send_time == 0:
+        if lang == "RU":
+            text = f"🔥 сломалось: {alert.text}"
+        else:
+            text = f"🔥 broken: {alert.text}"
+    else:
+        broken_time = format_seconds(time.time() - alert.start_time, lang=lang)
+        if lang == "RU":
+            text = f"⏱ сломано уже {broken_time}: {alert.text}"
+        else:
+            text = f"⏱ broken for {broken_time}: {alert.text}"
+
+    if len(text) > max_len:
+        text = text[:max_len][:-3] + "..."
+
+    return text
+
+
 def delete_alert(alert):
     if alert.alert_id in prefix_to_id_to_alert[alert.prefix]:
         del prefix_to_id_to_alert[alert.prefix][alert.alert_id]
@@ -149,25 +152,32 @@ def delete_alert(alert):
             del prefix_to_id_to_alert[alert.prefix]
 
 
-async def send_new_alerts():
-    filename_to_alerts = {}
+async def send_msg(user_id, text):
+    log("send_msg", user_id, text)
 
+    url = "https://api.telegram.org/bot%s/sendMessage" % BOT_TOKEN
+    payload = {"chat_id": user_id, "text": text,}
+
+    try:
+        async with httpx.AsyncClient() as client:
+            resp = httpx.post(url, json=payload)
+            if resp.status_code != 200:
+                log(f"Failed to send msg to {user_id}: {text} " +
+                    f"{resp.status_code} {resp.text}")
+                metrics.tg_fails += 1
+            return resp.status_code == 200
+    except OSError:
+        traceback.print_exc()
+        metrics.exceptions_cnt["alert_sender"] += 1
+        return False
+
+
+def get_sendable_alerts():
+    sendable_alerts = []
+
+    cur_time = time.time()
     for id_to_alert in prefix_to_id_to_alert.values():
-        for alert in id_to_alert.values():
-            if alert.filename not in filename_to_alerts:
-                filename_to_alerts[alert.filename] = []
-
-            for alert_id, alert in id_to_alert.items():
-                filename_to_alerts[alert.filename].append(alert)
-
-    for filename, alerts in filename_to_alerts.items():
-        msg = ""
-        alerts_in_send_batch = []
-
-        cur_time = time.time()
-
-        good_alerts = []
-        for a in alerts:
+        for a in id_to_alert.values():
             if not a.recovered and not prefix_to_checks_cnt[a.prefix] and a.prefix[1] != "__loading__":
                 # there was no checks after reloading
                 continue
@@ -182,45 +192,58 @@ async def send_new_alerts():
 
             if (a.recovered or a.last_send_time == 0 or
                     a.last_send_time + a.renotify < cur_time):
-                good_alerts.append(a)
+                sendable_alerts.append(a)
+
+    sendable_alerts.sort(key=lambda k: (k.recovered, k.last_send_time, k.start_time))
+    return sendable_alerts
 
 
-        good_alerts.sort(key=lambda k: (k.recovered, k.last_send_time, k.start_time))
+def get_alert_group(alert):
+    return alert.filename
 
-        for alert in good_alerts:
-            if alert.recovered:
-                broken_time = format_seconds(cur_time - alert.start_time, lang=LANGUAGE)
-                if LANGUAGE == "RU":
-                    msg_part = f"🎉 починилось, было сломано {broken_time}: {alert.text}"
-                else:
-                    msg_part = f"🎉 fixed, was broken {broken_time}: {alert.text}"
-            elif alert.last_send_time == 0:
-                if LANGUAGE == "RU":
-                    msg_part = f"🔥 сломалось: {alert.text}"
-                else:
-                    msg_part = f"🔥 broken: {alert.text}"
-            else:
-                broken_time = format_seconds(cur_time - alert.start_time, lang=LANGUAGE)
-                if LANGUAGE == "RU":
-                    msg_part = f"⏱ сломано уже {broken_time}: {alert.text}"
-                else:
-                    msg_part = f"⏱ broken for {broken_time}: {alert.text}"
+
+async def send_alerts(alerts):
+    group_to_alerts = {}
+    for alert in alerts:
+        group = get_alert_group(alert)
+        if group not in group_to_alerts:
+            group_to_alerts[group] = []
+        group_to_alerts[group].append(alert)
+
+    for group, alerts in group_to_alerts.items():
+        msg = ""
+        alerts_to_send = []
+
+        for alert in alerts:
+            msg_part = format_alert(alert, lang=LANGUAGE)
 
             if len(msg) + len(msg_part) + 1 >= MAX_TG_MSG_LEN:
                 break
 
             msg += msg_part + "\n"
-            alerts_in_send_batch.append(alert)
+            alerts_to_send.append(alert)
 
         if not msg:
             continue
 
         success = await send_msg(TG_DEST_ID, msg)
         if success:
-            for alert in alerts_in_send_batch:
-                alert.last_send_time = cur_time
-                if alert.recovered:
-                    delete_alert(alert)
+            for alert in alerts_to_send:
+                alert.last_send_time = time.time()
+
+
+async def send_new_alerts():
+    sendable_alerts = get_sendable_alerts()
+    send_start_time = time.time()
+
+    try:
+        await send_alerts(sendable_alerts)
+    finally:
+        # clean recovered alerts
+        for alert in sendable_alerts:
+            if alert.recovered and alert.last_send_time >= send_start_time:
+                delete_alert(alert)
+
 
 
 async def alert_sender_loop():
